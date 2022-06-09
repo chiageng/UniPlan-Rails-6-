8b2d8cb22e304ee0b6a3208eb5632bf68325925c
chiageng <91059092+chiageng@users.noreply.github.com>
2022-06-09 21:10:37 +0800

    Add category features and styling for timetable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     end
 
     def compare_time(work, starttime, endtime)
-        work.starttime.strftime( "%H%M%S%N" ) >= starttime.utc.strftime( "%H%M%S%N" ) && work.starttime.strftime( "%H%M%S%N" ) <= endtime.utc.strftime( "%H%M%S%N" )
+        (work.starttime.strftime( "%H%M%S%N" ) >= starttime.utc.strftime( "%H%M%S%N" ) && work.starttime.strftime( "%H%M%S%N" ) <= endtime.utc.strftime( "%H%M%S%N" )) || (work.endtime.strftime( "%H%M%S%N" ) >= starttime.utc.strftime( "%H%M%S%N" ) && work.endtime.strftime( "%H%M%S%N" ) <= endtime.utc.strftime( "%H%M%S%N" ))
         
     end 
 end
