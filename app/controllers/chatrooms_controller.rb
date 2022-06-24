d204db73c00254b144741d0ca4a9697ae7ed9dee
chiageng <91059092+chiageng@users.noreply.github.com>
2022-06-24 11:46:07 +0800

    Add form in chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -10,4 +10,24 @@ class ChatroomsController < ApplicationController
         @chatroom = Chatroom.find(params[:id])
         @messages = @chatroom.messages
     end 
+
+    def new 
+        @chatroom = Chatroom.new 
+    end 
+
+    def create 
+        @chatroom = Chatroom.new(chatroom_params)
+        if @chatroom.save 
+            flash[:success] = "A new chatroom is created successfully"
+            redirect_to @chatroom
+        else 
+            flash[:danger] = "Chatroom create is unsuccessfull"
+            render "new"
+        end 
+    end 
+
+    private
+    def chatroom_params
+        params.require(:chatroom).permit(:name)
+    end
 end
