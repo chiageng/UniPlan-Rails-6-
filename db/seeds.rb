fb7c91e4d274c4c7fdb822c93c0d057566d1e32c
chiageng <chiageng12@gmail.com>
2023-02-03 00:06:21 +0800

    Adding redis addon

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 # User.create(username: "admin1", email: "admin1@example.com", password: "admin1", admin: true)
 # User.create(username: "admin2", email: "admin2@example.com", password: "admin2", admin: true)
 # ForumCategory.destroy_all
-User.destroy_all
+# User.destroy_all
